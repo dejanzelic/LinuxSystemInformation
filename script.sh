626ff91018dc9ed40a5159e47b37eed54ec9dd71
dejan <dejanzelic@gmail.com>
2014-12-15 12:34:40 -0700

    Chapter 26 - 27 Added functions Check if current user is root

--- script.sh
+++ script.sh
@@ -5,6 +5,34 @@ TITLE="System Information Report For $HOSTNAME"
 CURRENT_TIME=$(date +"%x %r %Z")
 TIME_STAMP="Generated $CURRENT_TIME, by $USER"
 
+report_uptime () {
+    cat <<- _EOF_
+            <H2>System Uptime</H2>
+            <PRE>$(uptime)</PRE>
+_EOF_
+    return
+}
+report_disk_space(){
+    cat <<- _EOF_
+            <H2>Disk Space Utilization</H2>
+            <PRE>$(df -h)</PRE>
+_EOF_
+    return
+}
+report_home_space(){
+    if test $(id -u) == 0; then
+        cat <<- _EOF_
+                <H2>Home Space Utilization</H2>
+                <PRE>$(du -sh /home)</PRE>
+_EOF_
+    else
+         cat <<- _EOF_
+                    <H2>Home Space Utilization</H2>
+                    <PRE>$(du -sh $HOME)</PRE>
+_EOF_
+    fi
+    return
+}
 cat << _EOF_
  <HTML>
     <HEAD>
@@ -13,6 +41,9 @@ cat << _EOF_
     <BODY>
         <H1>$TITLE</H1>
         <p>$TIME_STAMP</p>
+        $(report_uptime)
+        $(report_disk_space)
+        $(report_home_space)
     </BODY>
 </HTML>
 _EOF_
